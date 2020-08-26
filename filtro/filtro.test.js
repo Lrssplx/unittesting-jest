471c3790ae1335b91bf191b017f8a9d74bfeea95
lrssplx <lrssplx@gmail.com>
2020-08-26 18:07:49 -0300

    feat(Adicionando arquivos): adcionando
    escape
    
    BREAKING CHANGE: Escape
    Closes Escape

---
+++ filtro/filtro.test.js
@@ -0,0 +1,12 @@
+const filtro = require('./filtro')
+const contatos = [{"nome":"Larissa", "sobrenome": "Pereira", "nome": "Caio", "sobrenome": "Pereira"}]
+const encontrado = {"nome":"Larissa", "sobrenome": "Pereira"}
+
+describe('Filtro', ()=> {
+    test('Pesquisar nome ', ()=> {
+        expect(filtro(contatos,"Larissa")).toEqual(encontrado)
+    })
+    test('Pesquisar nome que nao existe na base', ()=> {
+        expect(filtro(contatos, "xpto")).toBeUndefined()
+    });
+});
